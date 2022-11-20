3107c9814e95b68c2700c07345a28fc3eb4e028b
robvanvolt <robvanvolt@gmail.com>
2022-11-20 22:04:55 +0000

    alpha working release

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 cache_path = './cache'
 log_path = './logs'
 output_path = './output'
+target_path = 's3://s-laion/laion-2b-mobilenetv3'
 
 ## slurm configuration
 job_name = 'inferencepipeline'
@@ -10,11 +11,13 @@ n_nodes = 1
 gpus = 1
 cpus_per_gpu = 2
 ntasks_per_node = 8
-max_concurrent_processes = 8
+max_concurrent_processes = 2
 
 ## Webdataset being processed
 batch_size = 2
 dataset_urls = []
-for i in range(231350):
+shard_total = 231350
+
+for i in range(shard_total):
     dataset_urls.append(f's3://s-datasets/laion5b/laion2B-data/{i:06d}.tar')
 dataset_urls = [f'pipe:aws s3 cp {url} -' for url in dataset_urls]
--- slurm_job.py
+++ slurm_job.py
@@ -1,43 +1,90 @@
 import fire
 import webdataset as wds
-from torch.utils.data import DataLoader
-from config import cache_path, output_path
+from config import cache_path, output_path, target_path
 import os
 
+import subprocess
+import webdataset as wds
+import pandas as pd
+from torch.utils.data import DataLoader
+from itertools import islice
+import numpy as np
+from io import BytesIO
+# from pyrovision.models import model_from_hf_hub
+from torchvision.transforms import Compose, ConvertImageDtype, Normalize, PILToTensor, Resize, ToTensor, CenterCrop
+from torchvision.transforms.functional import InterpolationMode
+from torchvision import models
+import io
+from PIL import Image
+import torch
+
+def decodebyte(x):
+    return Image.open(io.BytesIO(x)).convert("RGB")
+
+transform = Compose([
+        Resize((224, 224)),
+        CenterCrop(224),
+        ToTensor(),
+        Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        ),
+])
+
 def worker(current_shard):
-
-    # Read environment variables
-    # These are set by slurm_distributor or SLURM itself
-    global_rank = int(os.environ["SLURM_PROCID"])
-    local_rank = int(os.environ["SLURM_LOCALID"])
-
-    print(f'global_rank: {global_rank}, local_rank: {local_rank}')
     try:
+        bs = 256
         dataset_url = f'pipe:aws s3 cp s3://s-datasets/laion5b/laion2B-data/{current_shard:06d}.tar -'
+        ds = wds.WebDataset(dataset_url, handler=wds.warn_and_continue).map_dict(jpg=decodebyte).map_dict(jpg=transform)
+        dl = DataLoader(ds, num_workers=1, batch_size=bs, pin_memory=True, shuffle=False)
+        device = "cuda" if torch.cuda.is_available() else "cpu"
 
-        ds = wds.WebDataset(dataset_url, handler=wds.ignore_and_continue)
-        dl = DataLoader(ds, num_workers=1, batch_size=1)
+        with torch.no_grad():
+            model = models.mobilenet_v3_small(pretrained=True).eval()
+            if torch.cuda.is_available():
+                model.cuda()
 
-        c = 0
+            mobilenet_gap_op = torch.nn.Sequential(
+                model.features, model.avgpool
+            )
 
-        for _ in dl:
-            c += 1
+            keys = []
+            urls = []
+            json_dict = []
+            txt = []
+            mobilenetv3_small = []
 
-        print(f"{current_shard:06d}: {c}")
+            for b in islice(dl, 3):
+                image_features = mobilenet_gap_op(b['jpg'].to(device))
+                mobilenetv3_small.extend(image_features.cpu().numpy())
+                keys.extend(b['__key__'])
+                urls.extend(b['__url__'])
+                json_dict.extend(b['json'])
+                txt.extend(b['txt'])
 
-        os.makedirs(output_path, exist_ok=True)
+        df = pd.DataFrame({'__key__': keys, '__url__': urls, 'json': json_dict, 'txt': txt})
+
+        # df.to_parquet(f'{output_path}/{current_shard:06d}.parquet')
+        df.to_parquet(f'{target_path}/{current_shard:06d}.parquet')
+
+        with open(f'{output_path}/{current_shard:06d}.npy', "wb") as f:
+            npb = BytesIO()
+            np.save(npb, np.asanyarray(mobilenetv3_small))
+            f.write(npb.getbuffer())
         
-        with open(f'{output_path}/{current_shard:06d}.txt', "w") as f:
-            f.write(f"{current_shard:06d}: {c}")
+        subprocess.run(["aws", "s3" , "cp", f'{output_path}/{current_shard:06d}.npy', f"{target_path}/{current_shard:06d}.npy"])
 
+        os.remove(f'{output_path}/{current_shard:06d}.npy')
 
     except Exception as e:
+        print(e)
         os.rename(cache_path + f"/sbatch_script_{current_shard:06d}.sh", cache_path + f"/sbatch_script_{current_shard:06d}_failed.sh")
         try:
             with open("error_logs.txt", "a") as f:
                 f.write(f"Shard number {current_shard:06d}:" + e)
-        except:
+        except Exception as e:
             print('Could not write error logs...')
+            print(e)
     else:
         os.remove(cache_path + f"/sbatch_script_{current_shard:06d}.sh")
     
